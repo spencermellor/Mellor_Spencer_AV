8ba9410b8e84b2b5878ff968788a325cb2725738
Spencer Mellor <spencermellor@MacBook-Pro-3.local>
2021-04-07 18:53:32 -0400

    added the ability to toggle a transcript for the users media choice, updated the readMe, and did a little css.

--- main.js
+++ main.js
@@ -100,3 +100,130 @@ function loopDurationUpdate() {
 }
 
 loopDurationUpdate()
+
+// transcript
+
+const transcriptDisplay = document.querySelector('#transcriptDisplay');
+const transcript = document.querySelector('#transcript');
+
+transcript.addEventListener('click', transcriptHandler);
+
+
+function transcriptHandler() {
+  if (transcriptDisplay.style.display == 'block') {
+    transcriptDisplay.style.display = 'none';
+  } else {
+    transcriptDisplay.style.display = 'block';
+    transcriptDisplay.children[1].textContent = transcripts[video.title];
+  }
+  
+
+
+
+}
+
+
+
+const transcripts = {
+  batman: `every punk in this town is scared, stiff.
+  They say he can't be killed.
+  They say he drinks.
+  Is there a six foot back in Gotham city?
+  
+  Nicky Vail
+  Bruce Wayne
+  What do you do for a living?
+  
+  The city without me,
+  knock is about to change
+  free terrorizes.
+  Really get a load of
+  me out there right now.
+  I've got to go to work.
+  I have given a name to my pain.`,
+  full_house: `
+(Ahhh, ahhh, ahhh, ahhh)
+Whatever happened to predictability
+The milkman, the paperboy, evening TV?
+How did I get delivered here?
+Somebody tell me, please
+This old world's confusing me
+
+With clouds as mean as you've ever seen
+Ain't a bird who knows your tune.
+Then a little voice inside you whispers,
+"Kid don't sell your dreams, so soon."
+
+Everywhere you look
+(Everywhere you look)
+There's a heart
+(There's a heart)
+A hand to hold onto
+Everywhere you look
+(Everywhere you look)
+There's a face
+Of somebody who needs you
+Everywhere you look
+
+When you're lost out there
+and you're all alone,
+A light is waiting
+to carry you home.
+Everywhere you look
+Everywhere you look
+(doop-a-dee-ba-ba-dow)
+
+  `,
+  cats_in_the_cradle: `
+  My child arrived just the other day
+He came to the world in the usual way
+But there were planes to catch, and bills to pay
+He learned to walk while I was away
+And he was talking 'fore I knew it, and as he grew
+He'd say "I'm gonna be like you, dad"
+"You know I'm gonna be like you"
+And the cat's in the cradle and the silver spoon
+Little boy blue and the man in the moon
+"When you coming home, dad?" "I don't know when"
+But we'll get together then
+You know we'll have a good time then
+My son turned ten just the other day
+He said, thanks for the ball, dad, come on let's play
+Can you teach me to throw, I said-a, not today
+I got a lot to do, he said, that's okay
+And he, he walked away, but his smile never dimmed
+It said, I'm gonna be like him, yeah
+You know I'm gonna be like him
+And the cat's in the cradle and the silver spoon
+Little boy blue and the man in the moon
+"When you coming home, dad?" "I don't know when"
+But we'll get together then
+You know we'll have a good time then
+Well, he came from college just the other day
+So much like a man I just had to say
+Son, I'm proud of you, can you sit for a while?
+He shook his head, and they said with a smile
+What I'd really like, dad, is to borrow the car keys
+See you later, can I have them please?
+And the cat's in the cradle and the silver spoon
+Little boy blue and the man in the moon
+"When you coming home, son?" "I don't know when"
+But we'll get together then, dad
+You know we'll have a good time then
+I've long since retired, my son's moved away
+I called him up just the other day
+I said, I'd like to see you if you don't mind
+He said, I'd love to, dad, if I can find the time
+You see, my new job's a hassle, and the kids have the flu
+But it's sure nice talking to you, dad
+It's been sure nice talking to you
+And as I hung up the phone, it occurred to me
+He'd grown up just like me
+My boy was just like me
+And the cat's in the cradle and the silver spoon
+Little boy blue and the man in the moon
+"When you coming home, son?" "I don't know when"
+But we'll get together then, dad
+We're gonna have a good time then
+  `
+}
